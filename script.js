8688713b105d7e45d43c2d4a0c863826415ce508
unknown <giutrum@gmail.com>
2024-10-08 14:45:43 -0300

    Implementa a etapa 2 de interface e finaliza o projeto

--- script.js
+++ script.js
@@ -1,3 +1,79 @@
+function criptografar() {
+    const mensagem1 = document.getElementById('criptografar')
+    const mensagemoriginal = mensagem1.value
+
+    const chave = document.getElementById('chave')
+    const chaveoriginal = chave.value.repeat(10)
+
+
+
+    const cifra = [];
+
+
+
+    const alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    for (posição = 0; posição < mensagemoriginal.length; posição++) {
+        for (i = 0; i < 26; i++) {
+            if (mensagemoriginal[posição].includes(alfabeto[i])) {
+                console.log(posição + ": " + alfabeto[i])
+                if ((alfabeto.indexOf(mensagemoriginal[posição]) + alfabeto.indexOf(chaveoriginal[posição])) < 26) {
+                    cifra.push(alfabeto[(alfabeto.indexOf(mensagemoriginal[posição]) + alfabeto.indexOf(chaveoriginal[posição]))])
+                    var cifraoriginal = cifra.join("");
+
+
+                }
+                else if ((alfabeto.indexOf(mensagemoriginal[posição]) + alfabeto.indexOf(chaveoriginal[posição])) >= 26) {
+                    cifra.push(alfabeto[(alfabeto.indexOf(mensagemoriginal[posição]) + alfabeto.indexOf(chaveoriginal[posição])) - 26])
+                    var cifraoriginal = cifra.join("");
+
+                }
+
+            }
+        }
+        
+    }
+
+    document.getElementById("resposta").innerHTML = cifraoriginal;
+}
+
+function descriptografar() {
+    const mensagem1 = document.getElementById('descriptografar')
+    const mensagemoriginal = mensagem1.value
+
+    const chave = document.getElementById('chave')
+    const chaveoriginal = chave.value.repeat(10)
+
+    const p1 = [];
+
+
+
+    const alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    for (posição = 0; posição < mensagemoriginal.length; posição++) {
+        for (i = 0; i < 26; i++) {
+            if (mensagemoriginal[posição].includes(alfabeto[i])) {
+                console.log(posição + ": " + alfabeto[i])
+                if ((alfabeto.indexOf(mensagemoriginal[posição]) - alfabeto.indexOf(chaveoriginal[posição])) < 0) {
+                    p1.push(alfabeto[(alfabeto.indexOf(mensagemoriginal[posição]) - alfabeto.indexOf(chaveoriginal[posição])) + 26])
+                    var p1original = p1.join("");
+                    console.log(p1original);
+
+                }
+                else if ((alfabeto.indexOf(mensagemoriginal[posição]) - alfabeto.indexOf(chaveoriginal[posição])) >= 0) {
+                    p1.push(alfabeto[(alfabeto.indexOf(mensagemoriginal[posição]) - alfabeto.indexOf(chaveoriginal[posição]))])
+                    var p1original = p1.join("");
+                    console.log(p1original);
+                }
+
+            }
+        }
+        
+    }
+    document.getElementById("resposta").innerHTML = p1original;
+    
+}
+
 
 function api() {
     const cifra = [];
